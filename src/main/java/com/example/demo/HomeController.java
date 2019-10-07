0af469155ddaad820652a4d2a085a500246a9296
Mel Belachew <melbelachew@gmail.com>
2019-10-07 16:29:59 -0400

    updated

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -9,6 +9,7 @@ public class HomeController {
     @RequestMapping("/")
     public String homepage (Model model){
         model.addAttribute("myvar", "say hello to the people.");
+        model.addAttribute("myvar2", "This is Mel from Spring Boot!");
         return "hometemplate";
     }
 }
